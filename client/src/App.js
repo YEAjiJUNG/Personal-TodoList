c1ef7cc07613bdf346e12a84c213545c45087711
YEAjiJUNG <happyhan96@naver.com>
2020-12-09 22:19:35 +0900

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -10,12 +10,13 @@ import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
 import Auth from './hoc/auth';
 
+//Route의 exact path는 경로가 완전히 일치할 때만 그 페이지로 이동
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path="/" component={Auth(LandingPage, null)} />
+          <Route exact path="/" component={Auth(LandingPage, null)} />  
           <Route path="/login" component={Auth(LoginPage, false)} />
           <Route path="/register" component={Auth(RegisterPage, false)} />
         </Switch>
