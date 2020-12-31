8290de60c24dc50bc1ad1385de6e390d34c81ddd
YEAjiJUNG <happyhan96@naver.com>
2020-12-31 23:05:29 +0900

    add TodoList.js file

--- client/src/components/views/TodoPage/TodoList.js
+++ client/src/components/views/TodoPage/TodoList.js
@@ -2,15 +2,34 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import TodoTemplatePage from './TodoTemplatePage';
 
-const TodoList = ({todos}) => {
+
+
+const TodoList = () => {
+
+    const todos = [
+        {
+          id: 1,
+          text: '리액트의 기초 알아보기',
+          checked: true,
+        },
+        {
+            id:2,
+            text:'컴포넌트 스타일링해 보기',
+            checked: true,
+        },
+        {
+            id:3,
+            text:'일정 관리 앱 만들어 보기',
+            checked: false,
+        },
+    ]
+
     return(
         <div className="TodoList">
             <TodoTemplatePage>
-            {todos.map(todo => {
-                <ul>
-                    <li>{todo}</li>
-                </ul>
-            })}
+            {todos.map((todo, id) =>(
+                <li key={id}>{todo.text}</li>
+            ))}
             </TodoTemplatePage>
         </div>
     )
