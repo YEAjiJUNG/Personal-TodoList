08f2513cd7b3cac4ff692df40dd41277fdd20517
YEAjiJUNG <happyhan96@naver.com>
2020-12-31 15:59:23 +0900

    add TodoList.js file

--- client/src/components/views/TodoPage/TodoList.js
+++ client/src/components/views/TodoPage/TodoList.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import TodoTemplatePage from './TodoTemplatePage';
 
-function TodoList({todos}){
+const TodoList = ({todos}) => {
     return(
         <div className="TodoList">
+            <TodoTemplatePage>
             {todos.map(todo => {
                 <ul>
                     <li>{todo}</li>
                 </ul>
             })}
+            </TodoTemplatePage>
         </div>
     )
 
 }   
+
+export default withRouter(TodoList);
