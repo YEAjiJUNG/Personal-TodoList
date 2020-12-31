a94632e7d79b8f37b96eab436e72e07cbeddd8fd
YEAjiJUNG <happyhan96@naver.com>
2020-12-31 23:47:02 +0900

    add TodoPage files

--- client/src/components/views/TodoPage/TodoTemplatePage.js
+++ client/src/components/views/TodoPage/TodoTemplatePage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import './TodoTemplatePage.css';
-import './TodoInsert';
+import TodoInsert from './TodoInsert';
 
-function TodoTemplatePage(){
+function TodoTemplatePage({children}){
     return(
         <div className="TodoTemplate">
             <div className="app-title">일정 관리</div>
-            <TodoInsert />
+            <div className="content"> {children} </div>
         </div>
     )
 }
