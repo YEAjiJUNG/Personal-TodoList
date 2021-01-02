50e6ad04ca011fe1bfd408537416395761fdb68f
YEAjiJUNG <happyhan96@naver.com>
2021-01-02 21:30:52 +0900

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -10,23 +10,11 @@ import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
 import Auth from './hoc/auth';
 import TodoList from './components/views/TodoPage/TodoList';
-import TodoInsert from './components/views/TodoPage/TodoInsert';
 
 //Route의 exact path는 경로가 완전히 일치할 때만 그 페이지로 이동
 function App() {
 
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2, 
-      text: '컴포넌트 스타일링해 보기',
-      checked: true
-    }
-  ])
+
 
   return (
     <Router>
@@ -36,7 +24,6 @@ function App() {
           <Route path="/login" component={Auth(LoginPage, false)} />
           <Route path="/register" component={Auth(RegisterPage, false)} />
           <Route path="/todolist" component={Auth(TodoList, true)} />
-          <Route path="/todoupdate" component={Auth(TodoInsert, true)} />
         </Switch>
       </div>
     </Router>
