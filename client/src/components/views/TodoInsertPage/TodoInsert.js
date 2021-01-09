acaf9063826acb8b450c6de3db2d7a8d8fb3d945
YEAjiJUNG <happyhan96@naver.com>
2021-01-09 13:31:53 +0900

    add TodoInsert.js file

--- client/src/components/views/TodoInsertPage/TodoInsert.js
+++ client/src/components/views/TodoInsertPage/TodoInsert.js
@@ -5,61 +5,39 @@ import { useLocation } from 'react-router-dom';
 import '../TodoPage/TodoTemplatePage.css';
 import TodoTemplatePage from '../TodoPage/TodoTemplatePage';
 import TodoInsertListItem from './TodoInsertListItem';
-import {MdAdd} from 'react-icons/md';
+import {MdAdd, MdAirlineSeatIndividualSuite} from 'react-icons/md';
 import './TodoInsert.scss';
 
 function TodoInsert(){
     const history = useHistory();
     const location = useLocation();
-
-    const [TodoList, setTodoList] = useState("");
-
-    const add = (TodoList) => {
-            const todo = {
-                TodoList
-            };
-            setTodoList(location.state.todolist.push(todo));
-            console.log(location.state.todolist)
+    // [TODO] Get Todolist from DB
+    // TODOLIST
+    const [todo, setTodo] = useState("");
+    console.log("First", TodoList);
+    const add = () => {
+            console.log("TEST", todo);
+            location.state.todolist.push({"body": todo});
+            //setTmpList(location.state.todolist);
+            setTodoList(location.state.todolist);
+            console.log("ADDing", TodoList);
     }
 
-    const onChange= useCallback(e => {
-        setTodoList(e.target.value)
+    const onChange = useCallback(e => {
+        setTodo(e.target.value);
     }, [])
 
-    /* const todos = [
-        {
-          id: 1,
-          text: '리액트의 기초 알아보기',
-          checked: true,
-        },
-        {
-            id:2,
-            text:'컴포넌트 스타일링해 보기',
-            checked: true,
-        },
-        {
-            id:3,
-            text:'일정 관리 앱 만들어 보기',
-            checked: false,
-        },
-        {
-            id:4,
-            text:'일정 관리 앱 만들어 보기',
-            checked: false,
-        },
-    ]
-    */
-  
+    console.log("List:", TodoList);
     return(
         <TodoTemplatePage>
-            <form className="TodoInsert">
-                <input placeholder="할 일을 입력하세요" value={TodoList} onChange={onChange}/>
-                <button type="submit" onClick={add}>
+            <form className="TodoInsert" onSubmit={add}>
+                <input placeholder="할 일을 입력하세요" value={todo} onChange={onChange}/>
+                <button type="submit">
                     <MdAdd />
                 </button>
             </form>
             <div className="TodoInsertList">
-                {location.state.todolist.map((todo, id) =>(
+                {TodoList.map((todo, id) =>(
                 <TodoInsertListItem key={id} todo={todo}></TodoInsertListItem>
                 ))}
             </div>
