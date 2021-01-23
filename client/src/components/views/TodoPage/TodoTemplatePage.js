0b8dcbf4ad6ec6c8d862c882dcc5dc55bc219784
YEAjiJUNG <happyhan96@naver.com>
2021-01-23 20:33:00 +0900

    fix warning

--- client/src/components/views/TodoPage/TodoTemplatePage.js
+++ client/src/components/views/TodoPage/TodoTemplatePage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import './TodoTemplatePage.scss';
-import { useHistory } from 'react-router-dom';
 
 function TodoTemplatePage({children}){
 
